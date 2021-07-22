26af1a3b67406cc1a8420d166dd7008d0c34da0e
rizki-P-star24 <83244066+rizki-P-star24@users.noreply.github.com>
2021-07-22 17:32:15 +0700

    draginput

--- resources/views/kelas1/matematika.blade.php
+++ resources/views/kelas1/matematika.blade.php
@@ -8,6 +8,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.2/dropzone.min.css" integrity="sha512-jU/7UFiaW5UBGODEopEqnbIAHOI8fO6T99m7Tsmqs2gkdujByJfkCbbfPSN4Wlqlb9TGnsuC0YgUgWkRBK7B9A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Hello, world!</title>
     <style>
          body:not(.mapelcaption){
@@ -453,8 +454,7 @@
                                           </div>
                                         </div>
                                       </div>
-                                    </div>
-
+                                  </div>
                               </div>
                           </div>
                           <table class="">
@@ -1586,7 +1586,7 @@
                       <h5 class="quiz-time fw-bold text-dark">{{$quiz->topik}}</h5>
                       <div class="d-flex">
                         <h6 class="quiz-time"><i class="fas fa-stopwatch"></i> {{$quiz->waktumulai}} WIB</h6>&nbsp;
-                        @if ($quiz->keterangan != Null)
+                        @if ($quiz->keterangan != NULL)
                         <a class="link-info fw-bold" style="margin-top: -3px; cursor: pointer;" data-bs-toggle="modal" data-bs-target="#keterangan{{$quiz->id}}"><i class="fas fa-info-circle"></i> info</a>
                         @endif
                          <!-- Modal -->
@@ -1760,12 +1760,16 @@
                   </div>
                     {{-- form-submitan = /kelas1/matematika/addsubmit --}}
                     <div class="col-8 container">
-                    <form class="container mb-2" action="/kelas1/matematika/addsubmit" enctype="multipart/form-data" method="post" >
+                    <form class="container mb-2" action="/kelas1/matematika/addsubmit" enctype="multipart/form-data" method="post" class="dropzone dz-clickable" >
                       {{ method_field('POST') }}
                       @csrf
                       <input type="hidden" name="nama" value="{{ Auth::user()->name }}">
                       <input type="hidden" name="submit_id" value="{{$sub->id}}">
-                      <input id="file" name="file" class="form-control form-sm" type="file"> <br>
+                      <input id="file" name="file" class="form-control form-sm dropzone" type="file">
+                      <div class="dz-default dz-message">
+                      <span class="text-muted fw-bold"><i class="fas fa-cloud-upload-alt"></i> Seret atau jatuhkan file disini</span>
+                      </div>
+                      <br>
                         <button type="submit" class="btn btn-outline-secondary kumpul-tugas{{$sub->id}}"><i class="fas fa-upload"></i> Kumpulkan Tugas</button>
                         @if (auth()->user()->level=="guru")
                         <a class="btn btn-outline-secondary" data-bs-toggle="offcanvas" href="#daftarsubmit{{$sub->id}}" role="button" aria-controls="offcanvasExample"><i class="fas fa-list-ul"></i> Daftar Submision</a>
@@ -1886,6 +1890,7 @@
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="../js/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.2/min/dropzone.min.js" integrity="sha512-VQQXLthlZQO00P+uEu4mJ4G4OAgqTtKG1hri56kQY1DtdLeIqhKUp9W/lllDDu3uN3SnUNawpW7lBda8+dSi7w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     @foreach ($submit as $sub)
     <script>
       $(document).ready(function () {
@@ -1933,6 +1938,12 @@
   
 </script> 
 @endforeach
+
+<script>
+  Dropzone.options.fileuploaded = {
+    maxFileSize: 1
+  }
+</script>
 
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
